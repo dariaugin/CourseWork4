9e940e7096b1f7eb77a49f6819bf678e38ac2443
dariaugin <39405713+dariaugin@users.noreply.github.com>
2022-12-28 11:24:21 +0100

    Initial Commit

---
+++ Course_Work_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Course_Work_Project
+//
+//  Created by Дарья Угнивенко on 28.12.2022.
+//  Copyright © 2022 dddd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
